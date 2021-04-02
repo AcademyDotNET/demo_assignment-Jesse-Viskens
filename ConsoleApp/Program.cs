9200f1abee66b120ba919e1719bc295e4497f598
Jesse <jesvis@bitconsult.be>
2021-04-02 11:51:55 +0200

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyFirstMethod();
+            SimpleMaths();
+        }
+
+        private static void SimpleMaths()
+        {
+            //make ex here
+        }
+
+        public static void MyFirstMethod()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
